39a02a9cbd6c9d8e15a858bf8f3b97a1da63f995
AyoubBF <ayoub.benfraj013@gmail.com>
2021-11-07 17:27:15 +0100

    changed tab to list

--- controllers/accounts.php
+++ controllers/accounts.php
@@ -123,95 +123,141 @@ function getBill()
 			$this->SetFillColor(247, 243, 243);
 			$this->SetTextColor(127, 122, 122);
 			$this->SetLineWidth(.3);
-			$this->SetXY(10,70);
+			$this->SetXY(40, 70);
 			$w = array(35, 35, 35, 35, 35);
 			for ($i = 0; $i < count($info_to_fill); $i++) {
+				$this->SetX(40);
 				$this->Cell($w[$i], 12, $info_to_fill[$i], 0, 1, 'C', true);
 			}
 			$this->SetFillColor(127, 122, 122);
-			$this->SetXY(12, 80);
+			$this->SetXY(42, 80);
 			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(12, 92);
+			$this->SetXY(42, 92);
 			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(12, 104);
+			$this->SetXY(42, 104);
 			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(12, 116);
+			$this->SetXY(42, 116);
 			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(12, 128);
+			$this->SetXY(42, 128);
 			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
 
 			$this->SetFillColor(247, 243, 243);
-			$this->SetXY(46,70);
+			$this->SetXY(86, 70);
 			$this->Cell(80, 12, $user_info['id'], 0, 1, 'C', true);
-			$this->SetX(46);
+			$this->SetX(86);
 			$this->Cell(80, 12, $user_info['nom'], 0, 1, 'C', true);
-			$this->SetX(46);
+			$this->SetX(86);
 			$this->Cell(80, 12, $user_info['email'], 0, 1, 'C', true);
-			$this->SetX(46);
+			$this->SetX(86);
 			$this->Cell(80, 12, $user_info['nomE'], 0, 1, 'C', true);
-			$this->SetX(46);
+			$this->SetX(86);
 			$this->Cell(80, 12, $user_info['adresseE'], 0, 1, 'C', true);
 
 			$this->SetFillColor(127, 122, 122);
-			$this->SetXY(48, 80);
+			$this->SetXY(88, 80);
 			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(48, 92);
+			$this->SetXY(88, 92);
 			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(48, 104);
+			$this->SetXY(88, 104);
 			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(48, 116);
+			$this->SetXY(88, 116);
 			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
 
-			$this->SetXY(48, 128);
+			$this->SetXY(88, 128);
 			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
-			
-
 		}
 
-		function FancyTable($header, $data)
+		function RentedVehicule($header, $data)
 		{
-			// Couleurs, épaisseur du trait et police grasse
-			$this->SetFillColor(255, 0, 0);
-			$this->SetTextColor(255);
-			$this->SetDrawColor(128, 0, 0);
+			$this->SetFillColor(247, 243, 243);
+			$this->SetTextColor(127, 122, 122);
 			$this->SetLineWidth(.3);
-			$this->SetFont('Times', 'B');
-			$this->SetXY(10, 140);
-			// En-tête
-			$w = array(40, 35, 45, 40);
+			$this->SetXY(84,145);
+			$this->Cell(40, 10, "Voiture a louer : ", 1, 0, 'C', false);
+			$this->SetXY(40, 160);
+			$w = array(35, 35, 35, 35, 35);
 			for ($i = 0; $i < count($header); $i++) {
-				$this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', true);
+				$this->SetX(40);
+				$this->Cell($w[$i], 12, $header[$i], 0, 1, 'C', true);
 			}
-			$this->Ln();
-			// Restauration des couleurs et de la police
-			$this->SetFillColor(224, 235, 255);
-			$this->SetTextColor(0);
-			$this->SetFont('');
-			// Données
-			$fill = false;
-			foreach ($data as $row) {
-				$this->Cell($w[0], 6, "", 'LR', 0, 'L', $fill);
-				$this->Cell($w[1], 6, "", 'LR', 0, 'L', $fill);
-				$this->Cell($w[2], 6, "", 'LR', 0, 'R', $fill);
-				$this->Cell($w[3], 6, "", 'LR', 0, 'R', $fill);
-				$this->Ln();
-				$fill = !$fill;
-			}
-			// Trait de terminaison
-			$this->Cell(array_sum($w), 0, '', 0, 1, 'T');
-			if(count($data)>10){
-				$this->Cell(array_sum($w), 10, 'Total a payer (avec reduction 10%)* : ', 'T'); //. $_SESSION['facture']['total'] / 10
-			} else {
-				$this->Cell(array_sum($w), 10, 'Total a payer (sans reduction 10%)* : ', 'T'); //. $_SESSION['facture']['total']
-			}
-			
+			$this->SetFillColor(127, 122, 122);
+			$this->SetXY(42, 170);
+			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
+
+			$this->SetXY(42, 182);
+			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
+
+			$this->SetXY(42, 194);
+			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
+
+			$this->SetXY(42, 206);
+			$this->Cell(31, 0.1, '', 0, 0, 'C', true);
+
+			$this->SetFillColor(247, 243, 243);
+			$this->SetXY(86, 160);
+			$this->Cell(80, 12, "", 0, 1, 'C', true);
+			$this->SetX(86);
+			$this->Cell(80, 12, "", 0, 1, 'C', true);
+			$this->SetX(86);
+			$this->Cell(80, 12, "", 0, 1, 'C', true);
+			$this->SetX(86);
+			$this->Cell(80, 12, "", 0, 1, 'C', true);
+
+			$this->SetFillColor(127, 122, 122);
+			$this->SetXY(88, 170);
+			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
+
+			$this->SetXY(88, 182);
+			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
+
+			$this->SetXY(88, 194);
+			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
+
+			$this->SetXY(88, 206);
+			$this->Cell(76, 0.1, '', 0, 0, 'C', true);
+
+			// // Couleurs, épaisseur du trait et police grasse
+			// $this->SetFillColor(255, 0, 0);
+			// $this->SetTextColor(255);
+			// $this->SetDrawColor(128, 0, 0);
+			// $this->SetLineWidth(.3);
+			// $this->SetFont('Times', 'B');
+			// $this->SetXY(10, 140);
+			// // En-tête
+			// $w = array(45, 45, 45, 45);
+			// for ($i = 0; $i < count($header); $i++) {
+			// 	$this->Cell($w[$i], 10, $header[$i], 1, 0, 'C', true);
+			// }
+			// $this->Ln();
+			// // Restauration des couleurs et de la police
+			// $this->SetFillColor(224, 235, 255);
+			// $this->SetTextColor(0);
+			// $this->SetFont('');
+			// // Données
+			// $fill = false;
+			// foreach ($data as $row) {
+			// 	$this->Cell($w[0], 12, "", 'LR', 0, 'L', $fill);
+			// 	$this->Cell($w[1], 12, "", 'LR', 0, 'L', $fill);
+			// 	$this->Cell($w[2], 12, "", 'LR', 0, 'R', $fill);
+			// 	$this->Cell($w[3], 12, "", 'LR', 0, 'R', $fill);
+			// 	$this->Ln();
+			// 	$fill = !$fill;
+			// }
+			// // Trait de terminaison
+			// $this->Cell(array_sum($w), 0, '', 0, 1, 'T');
+			// if(count($data)>10){
+			// 	$this->Cell(array_sum($w), 10, 'Total a payer (avec reduction 10%)* : ', 'T'); //. $_SESSION['facture']['total'] / 10
+			// } else {
+			// 	$this->Cell(array_sum($w), 10, 'Total a payer (sans reduction 10%)* : ', 'T'); //. $_SESSION['facture']['total']
+			// }
+
 		}
 
 		// Pied de page
@@ -221,7 +267,7 @@ function getBill()
 			$this->SetFontSize(12);
 			$this->SetTextColor(0, 0, 0);
 			$this->Cell(10, 10, '* Beneficiez d\'une reduction de 10% en louant au moins 10 voitures.', 0, 0);
-			
+
 			$this->SetY(-15);
 			$this->SetDrawColor(255, 0, 0);
 			$this->SetFillColor(255, 0, 0);
@@ -235,7 +281,7 @@ function getBill()
 	$pdf = new PDF();
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
-	$header = array('v.id', 'type', 'caract', 'prix');
+	$header = array('v.id :', 'type :', 'caract :', 'prix :');
 
 	require("./model/cars.php");
 	$Cars[] = description(1);
@@ -243,6 +289,6 @@ function getBill()
 	$info_to_fill = array('Identifiant :', 'Nom :', 'E-mail :', 'Entreprise :', 'Adresse :');
 	$user_info = $_SESSION['user_info'];
 	$pdf->EnterpriseInfos($info_to_fill, $user_info);
-	$pdf->FancyTable($header, $Cars);
+	$pdf->RentedVehicule($header, $Cars);
 	$pdf->Output();
 }
